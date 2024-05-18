d3b0c48264dcc31de0cfc13c15429054945b63f4
高見亨 <god.hexagon.bifry@gmail.com>
2024-05-18 12:02:13 +0900

    Contentコンポーザブルが重複している問題を解決

--- app/src/main/java/com/example/comikeapp/Content.fromTop.kt
+++ app/src/main/java/com/example/comikeapp/Content.fromTop.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Content(screenID: Int) {
@@ -19,6 +21,11 @@ fun Content(screenID: Int) {
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.End
     ) {
+        Text(
+            text = screenID.toString(),
+            color = Color.Black,
+            fontSize = 24.sp
+        )
         Box(
             modifier = Modifier
                 .size(width = 15.dp, height = 15.dp)
