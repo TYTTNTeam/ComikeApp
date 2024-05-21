de1f06d4f1597ef63e80f9ec6fc144f6696f7b69
takauotouki <tohki1028@gmail.com>
2024-05-21 13:45:25 +0900

    MapListItem(影苦戦中)修正中

--- app/src/main/java/com/example/comikeapp/MapList.ToMapListItem.kt
+++ app/src/main/java/com/example/comikeapp/MapList.ToMapListItem.kt
@@ -1,9 +1,7 @@
 package com.example.comikeapp
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.comikeapp.ui.theme.ComikeAppTheme
 
@@ -20,16 +18,19 @@ fun ToMapList() {
 @Composable
 fun ToMapListPreview() {
     ComikeAppTheme(darkTheme = false) {
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-                MapListItem(mapName = "地図地図",
-                    onNameChange = {},
-                    onDelete = {})
-                MapListItem(mapName = "地図2",
-                    onNameChange = {},
-                    onDelete = {})
+        Column {
+            MapListItem(mapName = "地図地図",
+                onNameChange = {},
+                onDelete = {})
+            MapListItem(mapName = "地図2",
+                onNameChange = {},
+                onDelete = {})
+            MapListItem(mapName = "地図3",
+                onNameChange = {},
+                onDelete = {})
+            MapListItem(mapName = "地図4",
+                onNameChange = {},
+                onDelete = {})
         }
     }
 }
