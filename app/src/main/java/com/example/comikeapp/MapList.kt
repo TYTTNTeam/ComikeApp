9bf5e1a8f0a06d11280914d674e2cc148355e3d3
takauotouki <tohki1028@gmail.com>
2024-05-28 12:30:46 +0900

    MapListレイアウト完成２

--- app/src/main/java/com/example/comikeapp/MapList.kt
+++ app/src/main/java/com/example/comikeapp/MapList.kt
@@ -12,9 +12,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -25,7 +23,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -83,7 +80,7 @@ fun MapList() {
                 selectedFileUri?.let { uri ->
                     //MapRegistDialog()
                 }
-                Image(painter = painterResource(R.drawable.buttonaddmap),
+                Image(painter = painterResource(R.drawable.button_add_map),
                     contentDescription = "BottomBarIcon",
                     modifier = Modifier
                         .padding(8.dp)
