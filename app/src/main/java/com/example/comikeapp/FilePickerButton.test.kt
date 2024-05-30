f2620f2516bbd42e7b6d901ca298afb7a0e32d3c
高見亨 <133332942+GodHexagon@users.noreply.github.com>
2024-05-30 11:44:33 +0900

    ファイル読み込み・表示テスト（成功）

--- app/src/main/java/com/example/comikeapp/FilePickerButton.test.kt
+++ app/src/main/java/com/example/comikeapp/FilePickerButton.test.kt
@@ -1,27 +1,48 @@
 package com.example.comikeapp
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import kotlinx.coroutines.delay
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
+import com.tom_roush.pdfbox.pdmodel.PDDocument
+import com.tom_roush.pdfbox.rendering.ImageType
+import com.tom_roush.pdfbox.rendering.PDFRenderer
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 @Composable
 fun FilePickerButton() {
-    var pickedImageUri by remember { mutableStateOf<String?>(null) }
+    var pickedImageUri by remember { mutableStateOf<ImageBitmap?>(null) }
+    val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
     val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         val uri = it.data?.data
         if (uri != null) {
-            coroutineScope.launch {
-                pickedImageUri = performAsyncTask(uri)
+            coroutineScope.launch(Dispatchers.IO) {
+                val file = convert(uri, context)
+                pickedImageUri = BitmapFactory.decodeFile(file?.path)?.asImageBitmap()
             }
         }
     }
@@ -37,14 +58,33 @@ fun FilePickerButton() {
         Text("Select")
     }
     pickedImageUri?.let {
-        Text(it)
+        Text(it.toString())
+        Image(bitmap = it, contentDescription = "test image")
     }
 }
 
-suspend fun performAsyncTask(uri: Uri): String {
-    // 非同期処理をここに記述します
-    // 例: URIからファイルを読み込んで処理する
-    delay(1000) // これはサンプルの遅延です。実際の非同期処理に置き換えてください。
-    Log.d("AsyncTask", "Async task completed with URI: $uri")
-    return uri.toString()
+suspend fun convert(uri: Uri, context: Context): File?{
+    try{
+        PDFBoxResourceLoader.init(context)
+
+        val document = PDDocument.load(context.contentResolver.openInputStream(uri))
+        val render = PDFRenderer(document)
+        val image = render.renderImage(0, 1f, ImageType.RGB)
+
+        val topDir = context.filesDir
+        val mapsDir = File(topDir, "maps")
+        mapsDir.mkdir()
+        val renderFile = File(mapsDir, "test")
+
+        val fileOut = FileOutputStream(renderFile)
+        image.compress(Bitmap.CompressFormat.JPEG, 100, fileOut)
+        fileOut.close()
+
+        Log.d("convert", "Successful rendering into $fileOut from $uri")
+        return renderFile
+    } catch (e: IOException) {
+        Log.e("convert", "Exception thrown while rendering file", e)
+        return null
+    }
+
 }
