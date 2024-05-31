00a838f8a6d5e19f98a9cdf36fe4eb82b150429a
高見亨 <133332942+GodHexagon@users.noreply.github.com>
2024-05-31 18:07:54 +0900

    MapImageRecorderクラスを作成・動作確認

---
+++ app/src/main/java/com/example/comikeapp/MapImageRecorder.kt
@@ -0,0 +1,57 @@
+package com.example.comikeapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.net.Uri
+import java.io.File
+import java.io.FileOutputStream
+
+class MapImageRecorder(private val context: Context) {
+    val dir: File = File(context.filesDir, "maps").apply {
+        if (!exists()) {
+            mkdir()
+        }
+    }
+    var renderFIle = this.dir
+
+    fun render(uri: Uri, fileName: String): File?{
+        context.contentResolver.openFileDescriptor(uri, "r")?.let { pfd ->
+            // PDFの1ページ目のインスタンスを生成
+            val renderer = PdfRenderer(pfd)
+            val page = renderer.openPage(0)
+            // 空のBitmapインスタンスを生成
+            val width = page.width
+            val height = page.height
+            val bitmapScale: Int = if (width < height) {
+                2400 / height
+            } else {
+                2400 / width
+            }
+            val bitmap = Bitmap.createBitmap(
+                width * bitmapScale,
+                height * bitmapScale,
+                Bitmap.Config.ARGB_8888
+            )
+            // Pageのメソッドで、Bitmapにレンダリングするよう依頼
+            page.render(
+                bitmap,
+                null,
+                null,
+                PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
+            )
+
+            page.close()
+            pfd.close()
+
+            // 空のファイルのインスタンスを生成
+            this.renderFIle = File(this.dir, fileName)
+            // Bitmapのメソッドで、画像を圧縮しつつ空のファイルに保存するよう依頼
+            val fileOut = FileOutputStream(this.renderFIle)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOut)
+            fileOut.close()
+            return this.renderFIle
+        }
+        return null
+    }
+}
