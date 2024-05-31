37eeed5a8c582103ee2d09010d9fc4232eebecc8
高見亨 <133332942+GodHexagon@users.noreply.github.com>
2024-05-31 20:30:26 +0900

    Revert "ファイル読み込みテスト"
    This reverts commit ec157b8f

--- app/src/main/java/com/example/comikeapp/MainActivity.kt
+++ app/src/main/java/com/example/comikeapp/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.comikeapp.ui.theme.ComikeAppTheme
-import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +26,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        PDFBoxResourceLoader.init(applicationContext)
     }
 }
