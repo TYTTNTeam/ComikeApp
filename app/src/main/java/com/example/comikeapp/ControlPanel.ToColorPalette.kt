d30013a01ab16acb7f64fd59dd9a1bede50390d2
ohagi-ky <uinnaasan@gmail.com>
2024-07-05 13:06:35 +0900

    ColorPaletteとColorRadioButton完成

--- app/src/main/java/com/example/comikeapp/ControlPanel.ToColorPalette.kt
+++ app/src/main/java/com/example/comikeapp/ControlPanel.ToColorPalette.kt
@@ -2,7 +2,6 @@ package com.example.comikeapp
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,7 +10,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ControlPanel() {
