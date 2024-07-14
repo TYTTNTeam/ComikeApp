877eb90be281de916ef348a25a2c08a6ac70c713
徳安弘隆 <hirotaka-toku2004@au.com>
2024-07-14 23:50:17 +0900

    ControlPanel完成しました ToDoを消した

--- app/src/main/java/com/example/comikeapp/MainActivity.kt
+++ app/src/main/java/com/example/comikeapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.comikeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.comikeapp.ui.theme.ComikeAppTheme
 
@@ -19,7 +21,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Top()
+                    Box(
+                        contentAlignment = Alignment.BottomCenter
+                    ) {
+                        test()
+                    }
+
                 }
             }
         }
---
+++ app/src/main/java/com/example/comikeapp/test.kt
@@ -0,0 +1,19 @@
+package com.example.comikeapp
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun test() {
+
+
+
+
+       /* ControlPanel(
+            onColorChange = {},
+            onThicknessChange = {},
+            onIntensityChange = {},
+            onChange = {}
+        ) */
+
+        ControlPanel(onChange = {})
+    }
