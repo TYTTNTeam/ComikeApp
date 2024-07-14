d9bfce212c7a30b1f8425c784c59e8250f07cfe6
徳安弘隆 <hirotaka-toku2004@au.com>
2024-07-14 23:41:37 +0900

    ControlPanel完成しました

--- app/src/main/java/com/example/comikeapp/PenControlPanel.kt
+++ app/src/main/java/com/example/comikeapp/PenControlPanel.kt
@@ -29,20 +29,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.comikeapp.ui.theme.ComikeAppTheme
 
 @Composable
-fun ControlPanel(modifier: Modifier = Modifier) {
+fun ControlPanel(
+    onColorChange: (Color) -> Unit,
+    onThicknessChange: (Float) -> Unit,
+    onIntensityChange: (Float) -> Unit,
+    onChange: () -> Unit ) {
+
     var penColor by remember { mutableStateOf(Color.Green) }
     var colorPaletteOpen by remember { mutableStateOf(false) }
     var penConfigurationOpen by remember { mutableStateOf(false) }
-    var eraserFunction by remember { mutableStateOf(true) }
     val screenWidth = LocalConfiguration.current.screenWidthDp
     var thickness by remember { mutableFloatStateOf(0.5f) }
     var intensity by remember { mutableFloatStateOf(1f) }
-
 
     Column {
 
@@ -86,6 +87,7 @@ fun ControlPanel(modifier: Modifier = Modifier) {
                     onClick = {
                         colorPaletteOpen = !colorPaletteOpen
                         penConfigurationOpen = false
+                        onColorChange(penColor)
                     },
                     shape = CircleShape,
                     modifier = Modifier
@@ -94,11 +96,13 @@ fun ControlPanel(modifier: Modifier = Modifier) {
                 ) {}
 
                 IconButton(
-                    onClick = { /* ToDo */ }
+                    onClick = { /*  */ },
+                    enabled = false
                 ) {}
 
                 IconButton(
-                    onClick = { /* ToDo */ }
+                    onClick = { /* ToDo */ },
+                    enabled = false
                 ) {
                 }
 
@@ -106,18 +110,31 @@ fun ControlPanel(modifier: Modifier = Modifier) {
                     onClick = {
                         penConfigurationOpen = !penConfigurationOpen
                         colorPaletteOpen = false
-                    }
+                        onThicknessChange(thickness)
+                        onIntensityChange(intensity)
+                    },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(50.dp)
                 ) {
                     Icon(
-                        imageVector = Icons.Filled.Tune,//Discover Tune
+                        imageVector = Icons.Filled.Tune,
                         contentDescription = "",
                         modifier = Modifier
                             .size(50.dp),
                         tint = MaterialTheme.colorScheme.background
                     )
                 }
+
                 IconButton(
-                    onClick = { /*TODO*/ }
+                    onClick = {
+                        onChange()
+                        colorPaletteOpen = false
+                        penConfigurationOpen = false
+                              },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(60.dp)
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Edit,
@@ -128,17 +145,6 @@ fun ControlPanel(modifier: Modifier = Modifier) {
                     )
                 }
             }
-        }
-    }
-}
-
-
-@Preview(showBackground = true, heightDp = 600, widthDp = 320)
-@Composable
-fun penPreview() {
-    ComikeAppTheme(darkTheme = false) {
-        Box(contentAlignment = Alignment.BottomCenter) {
-            ControlPanel()
         }
     }
 }
