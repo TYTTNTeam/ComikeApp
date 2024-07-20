9657ad34a4ae76d9110bf2ec5e3761021735d813
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-20 20:11:27 +0900

    テストファイルを追加

---
+++ app/src/androidTest/java/com/example/comikeapp/data/fileoperate/reserve/DeletingTest.kt
@@ -0,0 +1,44 @@
+package com.example.comikeapp.data.fileoperate.reserve
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class DeletingTest {
+    @Test
+    fun shouldDeleteAFile() {
+        val dir = InstrumentationRegistry.getInstrumentation().targetContext.filesDir
+        val log = mutableListOf<Boolean>()
+
+        val target = File(dir, "test.png")
+        log.add(target.exists())
+        target.createNewFile()
+        log.add(target.exists())
+        Deleting().access(target.toPath())
+        log.add(target.exists())
+
+        assertEquals(
+            log,
+            listOf(false, true, false)
+        )
+    }
+
+    @Test
+    fun shouldReturnFailedState(){
+        val dir = InstrumentationRegistry.getInstrumentation().targetContext.filesDir
+        val cannotFindFile = File(dir, "test.png")
+
+        assertFalse(Deleting().access(cannotFindFile.toPath()))
+    }
+}
