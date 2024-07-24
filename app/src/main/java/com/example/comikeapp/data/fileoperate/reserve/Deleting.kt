6ac7eee22d8e96c0ebcf6b743b187d783959bcdc
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-24 21:57:42 +0900

    LogCat出力を追加

--- app/src/main/java/com/example/comikeapp/data/fileoperate/reserve/Deleting.kt
+++ app/src/main/java/com/example/comikeapp/data/fileoperate/reserve/Deleting.kt
@@ -1,5 +1,6 @@
 package com.example.comikeapp.data.fileoperate.reserve
 
+import android.util.Log
 import java.io.File
 import java.nio.file.Path
 
@@ -7,7 +8,7 @@ class Deleting: Accessing() {
     override fun access(absolutePath: Path): Boolean {
         val target = File(absolutePath.toString())
         val success = target.delete()
-        if(success) this.accessedFile = target.toPath()
+        if(success) this.accessedFile = target.toPath() else Log.e("Deleting", "Failed accessing: deleting")
         return success
     }
 }
