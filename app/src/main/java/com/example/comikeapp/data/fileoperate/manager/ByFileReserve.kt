4b077ce3c1a56718dea7243521eb913444dd9d17
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-25 20:11:51 +0900

    初めて書き込みするとき、ディレクトリを作成するよう変更

--- app/src/main/java/com/example/comikeapp/data/fileoperate/manager/ByFileReserve.kt
+++ app/src/main/java/com/example/comikeapp/data/fileoperate/manager/ByFileReserve.kt
@@ -3,20 +3,26 @@ package com.example.comikeapp.data.fileoperate.manager
 import android.content.Context
 import android.util.Log
 import com.example.comikeapp.data.fileoperate.reserve.Accessing
+import com.example.comikeapp.data.fileoperate.reserve.Writing
 import java.io.File
+import kotlin.io.path.createParentDirectories
 
 class ByFileReserve(
     private val fileType: FileTypeDefinition,
     private val reserve: Accessing,
 ) {
     fun execute(context: Context, mapUUID: String): Boolean {
+        val absolutePath = File(
+            context.filesDir,
+            fileType.getRelativeDirFromMapUUID(mapUUID)
+        ).toPath()
+
+        if (reserve is Writing) {
+            absolutePath.createParentDirectories()
+        }
+
         return if (
-            reserve.access(
-                File(
-                    context.filesDir,
-                    fileType.getRelativeDirFromMapUUID(mapUUID)
-                ).toPath()
-            )
+            reserve.access(absolutePath)
         ) {
             true
         } else {
