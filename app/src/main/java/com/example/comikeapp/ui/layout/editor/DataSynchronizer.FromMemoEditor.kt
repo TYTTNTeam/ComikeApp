5f983aff91a73f3291eb7c29103f7e69780af387
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-26 19:51:25 +0900

    スタブを追加

---
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/DataSynchronizer.FromMemoEditor.kt
@@ -0,0 +1,36 @@
+package com.example.comikeapp.ui.layout.editor
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.comikeapp.data.maplist.MapList
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun DataSynchronizer(
+    modifier: Modifier = Modifier,
+    mapId: Int,
+    saving: Boolean,
+    onSavingChange: (Boolean) -> Unit,
+    content: @Composable (DrawingViewModel?, MapList?) -> Unit
+) {
+    var drawing by remember { mutableStateOf<DrawingViewModel?>(null) }
+    var map by remember { mutableStateOf<MapList?>(null) }
+
+    LaunchedEffect(key1 = saving) {
+        if(saving){
+            delay(1000)
+            drawing = DrawingViewModel()
+            map = MapList(0, "test map name", "test/path.png", null, null)
+            onSavingChange(false)
+        }
+    }
+
+    content(drawing, map)
+    // TODO 未実装
+}
