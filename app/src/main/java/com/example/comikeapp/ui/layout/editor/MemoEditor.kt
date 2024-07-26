01751a81cb6bb16117a4d768357aa629a457ba1b
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-26 19:51:13 +0900

    DrawingCanvasを描画できるように変更

--- app/src/main/java/com/example/comikeapp/ui/layout/editor/MemoEditor.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/MemoEditor.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -27,18 +32,49 @@ fun MemoEditor(
         mode = 0
     )) }
     var saving by remember { mutableStateOf(true) }
+    var awaitingSavingToClose by remember { mutableStateOf(false) }
     var mapName by remember { mutableStateOf("読み込み中...") }
 
     Box(modifier = modifier) {
+        DataSynchronizer(
+            mapId = mapId,
+            saving = saving,
+            onSavingChange = { newSaving ->
+                saving = newSaving
+                if(!newSaving && awaitingSavingToClose) onClose()
+            }
+        ) { drawing, map ->
+            if (map != null && drawing != null) {
+                LaunchedEffect(key1 = map) {
+                    mapName = map.mapName!!
+                }
+
+                DrawingCanvas(
+                    modifier = Modifier.fillMaxSize(),
+                    imagePath = map.imagePath!!,
+                    drawingData = drawing,
+                    penProperties = penProperties
+                )
+            } else {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center).size(100.dp),
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             TitleBar(
-                modifier = Modifier.height(66.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .height(66.dp)
+                    .fillMaxWidth(),
                 title = mapName,
                 onCloseRequest = {
-                    // TODO
+                    saving = true
+                    awaitingSavingToClose = true
                 }
             )
 
