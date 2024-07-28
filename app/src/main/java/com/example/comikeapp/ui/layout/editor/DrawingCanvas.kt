cde6e97c37a05acd5bd7dd072ff338740b2f8cba
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-28 11:31:18 +0900

    作り途中

---
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
@@ -0,0 +1,13 @@
+package com.example.comikeapp.ui.layout.editor
+
+import androidx.compose.runtime.Composable
+import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
+
+@Composable
+fun DrawingCanvas(
+    drawingData: DrawingViewModel,
+    imagePath: String,
+    penProperties: PenProperties
+) {
+
+}
---
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/StaticCanvas.kt
@@ -0,0 +1,98 @@
+package com.example.comikeapp.ui.layout.editor
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
+import com.example.comikeapp.data.viewmodel.editor.PathStyle
+
+@Composable
+fun StaticCanvas(viewModel: DrawingViewModel) {
+    var point by remember { mutableStateOf(Offset.Zero) } // point位置追跡のためのState
+    val points = remember { mutableListOf<Offset>() } // 新しく描かれた path を表示するためのPoints State
+
+    var path by remember { mutableStateOf(Path()) } // 新しく描かれている一画State
+
+    val pathStyle by viewModel.pathStyle.observeAsState()
+
+    val paths by viewModel.paths.observeAsState()
+
+    paths?.let {
+        Canvas(
+            modifier = Modifier
+                .height(650.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .clipToBounds()
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragStart = { offset ->
+                            point = offset
+                            points.add(point)
+                        },
+                        onDrag = { _, dragAmount ->
+                            point += dragAmount
+                            points.add(point)
+                            path = Path()
+                            points.forEachIndexed { index, point ->
+                                if (index == 0) {
+                                    path.moveTo(point.x, point.y)
+                                } else {
+                                    path.lineTo(point.x, point.y)
+                                }
+                            }
+                        },
+                        onDragEnd = {
+                            viewModel.addPath(Pair(path, pathStyle!!.copy()))
+                            points.clear()
+
+                            path = Path()
+                        }
+                    )
+                },
+
+            ) {
+            paths?.forEach { pair ->
+                drawPath(
+                    path = pair.first,
+                    style = pair.second
+                )
+            }
+
+            drawPath(
+                path = path,
+                style = pathStyle!!
+            )
+
+        }
+    }
+}
+
+internal fun DrawScope.drawPath(
+    path: Path,
+    style: PathStyle
+) {
+    drawPath(
+        path = path,
+        color = style.color,
+        alpha = style.alpha,
+        style = Stroke(width = style.width)
+    )
+}
