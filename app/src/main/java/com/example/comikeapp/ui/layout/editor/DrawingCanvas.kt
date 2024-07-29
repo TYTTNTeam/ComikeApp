697a6a6dc0903bfe24f5367e4864068959874739
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-29 22:17:18 +0900

    新しいコンポーザブルでラップした

--- app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
@@ -1,122 +1,58 @@
 package com.example.comikeapp.ui.layout.editor
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.core.content.ContextCompat
 import com.example.comikeapp.R
 import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
 
 @Composable
-fun DrawingCanvas(
+fun DrawingCanvas (
     modifier: Modifier = Modifier,
     drawingData: DrawingViewModel,
     imagePath: String,
     penProperties: PenProperties
-) {
-    var scale by remember { mutableStateOf(1f) }
-    var offset by remember { mutableStateOf(Offset.Zero) }
-    var toCenter by remember { mutableStateOf(Offset.Zero) }
+){
+    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
 
-    val isZoomable by drawingData.isZoomable.observeAsState()
+    val appContext = LocalContext.current
 
-    fun onPinchGesture(change1: PointerInputChange, change2: PointerInputChange) {
-        val distanceStart =
-            (change1.previousPosition - change2.previousPosition).getDistance()
-        val distanceEnd =
-            (change1.position - change2.position).getDistance()
-
-        val prev = scale
-
-        val newScale = scale * (distanceEnd / distanceStart)
-        scale =
-            newScale.coerceIn(0.5f, 50f) // スケールの最小・最大値を設定
-
-        val relativeScale = scale / prev
-
-        val midpoint =
-            (change1.position + change2.position) / 2f
-
-        val movedAbsolute =
-            offset + (midpoint - (change1.previousPosition + change2.previousPosition) / 2f)
-
-        val movedCenterAbsolute = movedAbsolute + toCenter
-        val midToMovedCenter = (movedCenterAbsolute - midpoint)
-
-        offset = movedAbsolute + midToMovedCenter * (relativeScale - 1)
+    ScalableCanvas(modifier = modifier, drawingData = drawingData, penProperties = penProperties) {
+        Image(
+            painter = painterResource(id = R.drawable.button_pen_change),
+            contentDescription = "test",
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+        )
     }
+}
 
-    LaunchedEffect(key1 = penProperties) {
-        drawingData.updateAlpha(penProperties.intensity)
-        drawingData.updateWidth(penProperties.thickness * 20)
-        drawingData.updateColor(penProperties.color)
-    }
-
-    isZoomable?.let {
-        Box(
-            modifier = modifier
-                .onSizeChanged { size ->
-                    toCenter = Offset(
-                        x = size.width / 2f,
-                        y = size.height / 2f
-                    )
-                }
-                .pointerInput(Unit) {
-                    awaitPointerEventScope {
-                        while (true) {
-                            val event = awaitPointerEvent()
-                            val changes = event.changes
-
-                            // ピンチ操作を検出
-                            val pinchMode = changes.size >= 2
-
-                            drawingData.setIsZoomable(pinchMode)
-
-                            if (pinchMode) {
-                                val change1 = changes[0]
-                                val change2 = changes[1]
-
-                                if (change1.pressed && change2.pressed) {
-                                    onPinchGesture(change1, change2)
-                                }
-                            }
-                        }
-                    }
-                }
-        ) {
-            Box(
-                modifier = Modifier
-                    .graphicsLayer(
-                        scaleX = scale,
-                        scaleY = scale,
-                        translationX = offset.x,
-                        translationY = offset.y
-                    )
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.button_pen_change),
-                    contentDescription = "test",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Yellow)
-                )
-                StaticCanvas(viewModel = drawingData)
-            }
-        }
-    }
+fun vectorDrawableToImageBitmap(@DrawableRes drawableRes: Int, context: Context): ImageBitmap {
+    val drawable = ContextCompat.getDrawable(context, drawableRes)
+    val bitmap = Bitmap.createBitmap(
+        100,
+        100,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    drawable?.setBounds(0, 0, canvas.width, canvas.height)
+    drawable?.draw(canvas)
+    return bitmap.asImageBitmap()
 }
---
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/ScalableCanvas.kt
@@ -0,0 +1,106 @@
+package com.example.comikeapp.ui.layout.editor
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
+
+@Composable
+fun ScalableCanvas(
+    modifier: Modifier = Modifier,
+    drawingData: DrawingViewModel,
+    penProperties: PenProperties,
+    backGround: @Composable() (BoxScope.() -> Unit)
+) {
+    var scale by remember { mutableStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    var toCenter by remember { mutableStateOf(Offset.Zero) }
+
+    fun onPinchGesture(change1: PointerInputChange, change2: PointerInputChange) {
+        val distanceStart =
+            (change1.previousPosition - change2.previousPosition).getDistance()
+        val distanceEnd =
+            (change1.position - change2.position).getDistance()
+
+        val prev = scale
+
+        val newScale = scale * (distanceEnd / distanceStart)
+        scale =
+            newScale.coerceIn(0.5f, 50f) // スケールの最小・最大値を設定
+
+        val relativeScale = scale / prev
+
+        val midpoint =
+            (change1.position + change2.position) / 2f
+
+        val movedAbsolute =
+            offset + (midpoint - (change1.previousPosition + change2.previousPosition) / 2f)
+
+        val movedCenterAbsolute = movedAbsolute + toCenter
+        val midToMovedCenter = (movedCenterAbsolute - midpoint)
+
+        offset = movedAbsolute + midToMovedCenter * (relativeScale - 1)
+    }
+
+    LaunchedEffect(key1 = penProperties) {
+        drawingData.updateAlpha(penProperties.intensity)
+        drawingData.updateWidth(penProperties.thickness * 20)
+        drawingData.updateColor(penProperties.color)
+    }
+
+    Box(
+        modifier = modifier
+            .onSizeChanged { size ->
+                toCenter = Offset(
+                    x = size.width / 2f,
+                    y = size.height / 2f
+                )
+            }
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent()
+                        val changes = event.changes
+
+                        // ピンチ操作を検出
+                        val pinchMode = changes.size >= 2
+
+                        drawingData.setIsZoomable(pinchMode)
+
+                        if (pinchMode) {
+                            val change1 = changes[0]
+                            val change2 = changes[1]
+
+                            if (change1.pressed && change2.pressed) {
+                                onPinchGesture(change1, change2)
+                            }
+                        }
+                    }
+                }
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+        ) {
+            backGround()
+            StaticCanvas(viewModel = drawingData)
+        }
+    }
+}
