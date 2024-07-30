5f38a427b61952bd493c52fd050fc5abe1543070
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-30 20:09:20 +0900

    コードを整形

--- app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
@@ -6,16 +6,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
-import com.example.comikeapp.R
 import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
 
 @Composable
@@ -25,8 +19,6 @@ fun DrawingCanvas (
     background: ImageBitmap,
     penProperties: PenProperties
 ){
-    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
-
     LaunchedEffect(key1 = penProperties) {
         drawingData.updateAlpha(penProperties.intensity)
         drawingData.updateWidth(penProperties.thickness * 20)
