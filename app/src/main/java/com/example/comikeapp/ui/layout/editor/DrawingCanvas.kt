65930e1c5e925ea839ead1f1306d34913b7b2a75
高見亨 <god.hexagon.bifry@gmail.com>
2024-07-30 22:45:44 +0900

    意図しないクリッピングを修正

--- app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/DrawingCanvas.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -55,14 +56,12 @@ fun DrawingCanvas(
                 )
                 Log.d("test", "onSizeChanged: ${size.width}, ${size.height}\n$scalableSize") // TODO
             }
-            .background(Color.Green),
+            .offset(),
         drawingData = drawingData,
         scalableSize = scalableSize
     ) { scale, offset ->
         Box(
             modifier = Modifier
-                .verticalScroll(scroll, false)
-                .horizontalScroll(scroll, false)
                 .width(with(density) { scalableSize.x.toDp() })
                 .height(with(density) { scalableSize.y.toDp() })
                 .graphicsLayer(
