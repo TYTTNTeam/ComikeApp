022fd91fbd6bc92bbb207d3b6b4993499f686340
高見亨 <god.hexagon.bifry@gmail.com>
2024-08-03 22:02:02 +0900

    作成される画像の大きさに制限を設けた

--- app/src/main/java/com/example/comikeapp/data/fileoperate/reserve/SynthesizingMap.kt
+++ app/src/main/java/com/example/comikeapp/data/fileoperate/reserve/SynthesizingMap.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.core.graphics.scale
 import com.example.comikeapp.data.editorrendering.mapMemoRendering
 import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
 import java.io.FileNotFoundException
@@ -16,22 +17,36 @@ class SynthesizingMap(
     private val drawing: DrawingViewModel
 ) : Writing() {
     override fun access(absolutePath: Path): Boolean {
-        val width = drawing.canvasSizePx.value!!.x.toInt()
-        val height =  drawing.canvasSizePx.value!!.y.toInt()
+        val canvasSizePx = drawing.canvasSizePx.value!!
 
         // Bitmapを作成し、Canvasに描画
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val bitmap = Bitmap.createBitmap(canvasSizePx.x.toInt(), canvasSizePx.y.toInt(), Bitmap.Config.ARGB_8888)
         Canvas(bitmap.asImageBitmap()).mapMemoRendering(
             drawing.paths.value!!.toList(),
             image,
             drawing.canvasSizePx.value!!.x / image.width
         )
 
+        val scale: Float
+
+        val maxSizePx = 2400
+        val aspect = canvasSizePx.x / canvasSizePx.y
+        scale = if(aspect < 1) {
+            maxSizePx / canvasSizePx.x
+        }else{
+            maxSizePx / canvasSizePx.y
+        }
+
+        val scaled = bitmap.scale(
+            (canvasSizePx.x * scale).toInt(),
+            (canvasSizePx.y * scale).toInt()
+        )
+
         val file = absolutePath.toFile()
 
         try {
             FileOutputStream(file).use { out ->
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+                scaled.compress(Bitmap.CompressFormat.PNG, 100, out)
             }
             accessedFile = absolutePath
             return true
