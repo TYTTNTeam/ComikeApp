8d8b99acb184fd91e7e59c241092590ca1d802e3
高見亨 <god.hexagon.bifry@gmail.com>
2024-08-03 21:09:47 +0900

    SynthesizingMapPreviewを変更

--- app/src/androidTest/java/com/example/comikeapp/data/fileoperate/reserve/SynthesizingMapPreview.kt
+++ app/src/androidTest/java/com/example/comikeapp/data/fileoperate/reserve/SynthesizingMapPreview.kt
@@ -1,8 +1,15 @@
 package com.example.comikeapp.data.fileoperate.reserve
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -10,19 +17,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.comikeapp.R
-import com.example.comikeapp.data.editorrendering.PathStyle
 import com.example.comikeapp.data.maplist.MapList
 import com.example.comikeapp.data.viewmodel.editor.DrawingViewModel
 import com.example.comikeapp.ui.layout.editor.DrawingCanvas
 import com.example.comikeapp.ui.layout.editor.PenProperties
-import com.example.comikeapp.ui.layout.editor.vectorDrawableToImageBitmap
 import com.example.comikeapp.ui.theme.ComikeAppTheme
 import java.io.File
 
@@ -35,24 +43,20 @@ fun SynthesizingMapPreview() {
     var map by remember { mutableStateOf<MapList?>(null) }
     var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
     var readImage by remember { mutableStateOf<ImageBitmap?>(null) }
+    var saving by remember { mutableStateOf(false) }
 
     val testViewmodelInstance: DrawingViewModel = viewModel()
 
     LaunchedEffect(key1 = Unit) {
-        val p = Path().apply {
-            moveTo(0f, 0f)
-            lineTo(5000f, 5000f)
-        }
-
-        testViewmodelInstance.addPath(Pair(p, PathStyle(color = Color.White)))
+        testViewmodelInstance.setCanvasSizePx(Offset(5000f, 5000f))
 
         drawing = testViewmodelInstance
         map = MapList(0, "test map name", "test/path.png")
         imageBitmap = vectorDrawableToImageBitmap(R.drawable.button_pen_change, appContext)
     }
 
-    LaunchedEffect(key1 = imageBitmap) {
-        imageBitmap?.let {
+    LaunchedEffect(key1 = saving) {
+        if (saving) {
             val target = File(appContext.dataDir, "test.png")
 
             val s = SynthesizingMap(imageBitmap!!, drawing!!)
@@ -61,6 +65,8 @@ fun SynthesizingMapPreview() {
             val r = ReadingImage()
             r.access(target.toPath())
             readImage = r.getData()
+
+            saving = false
         }
     }
 
@@ -70,12 +76,39 @@ fun SynthesizingMapPreview() {
                 .fillMaxSize()
         ) {
             imageBitmap?.let {
-                DrawingCanvas(drawingData = drawing!!, background = imageBitmap!!, penProperties = PenProperties(
-                    Color.Unspecified, 1f, 1f, 0))
-            }
-            readImage?.let {
-                Image(bitmap = readImage!!, contentDescription = "")
+                DrawingCanvas(
+                    modifier = Modifier.height(400.dp),
+                    drawingData = drawing!!,
+                    background = imageBitmap!!,
+                    penProperties = PenProperties(Color.Unspecified, 2f, 2f, 0)
+                )
+                Image(
+                    modifier = Modifier
+                        .height(400.dp)
+                        .clickable { saving = true },
+                    bitmap = if(readImage != null) readImage!! else imageBitmap!!,
+                    contentDescription = ""
+                )
             }
         }
     }
+}
+
+private fun vectorDrawableToImageBitmap(@DrawableRes drawableRes: Int, context: Context): ImageBitmap {
+    val drawable = ContextCompat.getDrawable(context, drawableRes)
+    val bitmap = Bitmap.createBitmap(
+        1000,
+        1300,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+
+    val paint = Paint().apply {
+        color = android.graphics.Color.YELLOW
+    }
+    canvas.drawRect(0f, 0f, canvas.width.toFloat(), canvas.height.toFloat(), paint)
+
+    drawable?.setBounds(0, 0, canvas.width, canvas.height)
+    drawable?.draw(canvas)
+    return bitmap.asImageBitmap()
 }
