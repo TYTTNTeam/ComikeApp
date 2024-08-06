24b38b5ad222fd28354995edca8b32c4e0851afb
ohagi-ky <uinnaasan@gmail.com>
2024-08-06 13:46:24 +0900

    MapRegistrationSequencer作成中2

--- app/src/main/java/com/example/comikeapp/ui/layout/menu/MapRegistrationSequencer.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/menu/MapRegistrationSequencer.kt
@@ -1,61 +1,3 @@
-/*package com.example.comikeapp.ui.layout.menu
-
-import android.content.Context
-import android.net.Uri
-import com.example.comikeapp.data.mapimagefile.MapImageCleaner
-import com.example.comikeapp.data.mapimagefile.MapImageRecorder
-import com.example.comikeapp.data.maplist.MapList
-import com.example.comikeapp.data.maplist.MapListDatabaseProvider
-import com.example.comikeapp.data.maplist.MapListRepository
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import java.util.UUID
-
-class MapRegistrationSequencer {
-    private val confirmName = CompletableDeferred<String?>()
-
-    suspend fun register(
-        scope: CoroutineScope,
-        appContext: Context,
-        pdf: Uri,
-        onComplete: (List<MapList>) -> Unit
-    ){
-        val mir = MapImageRecorder(appContext)
-        val renderImage = scope.async{
-            val uuid = UUID.randomUUID()
-            val newMap = mir.render(pdf, uuid.toString())
-            newMap
-        }
-
-        val imageFile = renderImage.await()
-        val name = this.confirmName.await()
-        if(name == null){
-            mir.rollback()
-        }else {
-            val db = MapListRepository(MapListDatabaseProvider.getDatabase(appContext).mapListDao())
-            val list = db.insertAndGetAll(name, imageFile.path)
-
-            onComplete(list)
-
-            scope.launch {
-                val paths = list.map { it.imagePath!! }
-                val cleaner = MapImageCleaner(appContext)
-                cleaner.clean(paths)
-            }
-        }
-    }
-
-    fun confirmName(name: String, confirm: Boolean){
-        if(confirm){
-            this.confirmName.complete(name)
-        }else{
-            this.confirmName.complete(null)
-        }
-    }
-}
-*/
 package com.example.comikeapp.ui.layout.menu
 
 import android.content.Context
@@ -64,6 +6,7 @@ import com.example.comikeapp.data.fileoperate.manager.ByFileReserve
 import com.example.comikeapp.data.fileoperate.manager.FileTypeDefinition
 import com.example.comikeapp.data.fileoperate.reserve.ConvertingImage
 import com.example.comikeapp.data.fileoperate.reserve.Deleting
+import com.example.comikeapp.data.mapimagefile.MapImageCleaner
 import com.example.comikeapp.data.maplist.MapList
 import com.example.comikeapp.data.maplist.MapListDatabaseProvider
 import com.example.comikeapp.data.maplist.MapListRepository
@@ -83,10 +26,7 @@ class MapRegistrationSequencer {
         onComplete: (List<MapList>) -> Unit
     ) {
         val mapUUID = UUID.randomUUID().toString()
-        val fileTypeDefinition = FileTypeDefinition(//TODO ここから
-            type = "map_images",
-            getRelativeDirFromMapUUID = { uuid -> "maps/$uuid" }
-        )//TODO ここまで
+        val fileTypeDefinition = FileTypeDefinition("raw_image")
         val convertingImage = ConvertingImage(pdf, appContext)
 
         val imageFilePath = scope.async {
@@ -110,9 +50,9 @@ class MapRegistrationSequencer {
             onComplete(list)
 
             scope.launch {
-                val paths = list.map { it.imagePath!! }//TODO
-                val cleaner = ByFileReserve(fileTypeDefinition, Deleting())
-                cleaner.execute(appContext, mapUUID)
+                val paths = list.map { it.imagePath!! }
+                val cleaner = MapImageCleaner(appContext)
+                cleaner.clean(paths)
             }
         }
     }
