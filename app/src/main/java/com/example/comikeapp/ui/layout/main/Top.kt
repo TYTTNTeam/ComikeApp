27099d4b60ab8ec8ecedc1765b2ab515bdaabe95
ohagi-ky <uinnaasan@gmail.com>
2024-08-07 21:48:38 +0900

    "変更mainグループ地図の初期表示"のタスクを行いました。 前回開いていた地図が表示されるよう変更しました。

--- app/src/main/java/com/example/comikeapp/ui/layout/main/Top.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/main/Top.kt
@@ -1,34 +1,56 @@
 package com.example.comikeapp.ui.layout.main
 
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Top() {
     var screenID by remember { mutableStateOf(0) }
-    var selectedMapId by remember { mutableStateOf(0) }
+    var selectedMapId by remember { mutableStateOf<Int?>(null) }
     val isEditorVisible = screenID == -1
+    val context = LocalContext.current
+
+    LaunchedEffect(selectedMapId) {
+
+        selectedMapId?.let {
+            val sharedPreferences =
+                context.getSharedPreferences("topInitialise", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putInt("selectedMapId", it)
+            editor.apply()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        val sharedPreferences = context.getSharedPreferences("topInitialise", Context.MODE_PRIVATE)
+        selectedMapId = sharedPreferences.getInt("selectedMapId", 0)
+    }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = if (isEditorVisible) 0.dp else BottomBarHeightDp)
     ) {
-        Content(
-            screenID = screenID,
-            selectedMapId = selectedMapId,
-            onShowMemoEditor = { isVisible ->
-                screenID = if (isVisible) -1 else 0
-            },
-            onMapIdChange = { mapId -> // 追加：地図ID変更時の処理
-                selectedMapId = mapId
-            }
-        )
+        selectedMapId?.let {
+            Content(
+                screenID = screenID,
+                selectedMapId = it,
+                onShowMemoEditor = { isVisible ->
+                    screenID = if (isVisible) -1 else 0
+                },
+                onMapIdChange = { mapId -> // 追加：地図ID変更時の処理
+                    selectedMapId = mapId
+                }
+            )
+        }
+
     }
 
     if (!isEditorVisible) {
