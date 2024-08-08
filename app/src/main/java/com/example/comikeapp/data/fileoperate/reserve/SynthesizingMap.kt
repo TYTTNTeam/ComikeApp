2daa06c8e4956f33ae4631b92397a5924725362d
高見亨 <god.hexagon.bifry@gmail.com>
2024-08-08 09:34:47 +0900

    間違ったアルゴリズムを修正

--- app/src/main/java/com/example/comikeapp/data/fileoperate/reserve/SynthesizingMap.kt
+++ app/src/main/java/com/example/comikeapp/data/fileoperate/reserve/SynthesizingMap.kt
@@ -24,13 +24,14 @@ class SynthesizingMap(
         Canvas(bitmap.asImageBitmap()).mapMemoRendering(
             drawing.paths.value!!.toList(),
             image,
-            drawing.canvasSizePx.value!!.x / image.width
+            canvasSizePx.x / image.width
         )
 
         val scale: Float
 
+        // 長編が一定サイズを超えないようにしている。
         val maxSizePx = 4800
-        val aspect = canvasSizePx.x / canvasSizePx.y
+        val aspect = canvasSizePx.y / canvasSizePx.x
         scale = if(aspect < 1) {
             maxSizePx / canvasSizePx.x
         }else{
