89e52ade6794d60ad0888232a053dc457cfdfff8
高見亨 <god.hexagon.bifry@gmail.com>
2024-08-08 09:10:03 +0900

    importを最適化

--- app/src/main/java/com/example/comikeapp/ui/layout/editor/MemoEditor.kt
+++ app/src/main/java/com/example/comikeapp/ui/layout/editor/MemoEditor.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
